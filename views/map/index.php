00199ecf03be9a3b5dd765264fe200a009c80cd9
Daryl <experth6@gmail.com>
2017-01-15 14:40:11 +0800

    Create index.php
    This will show all the Latitude, Longitude save from Database.

---
+++ views/map/index.php
@@ -0,0 +1,23 @@
+
+/*
+*  Codeigniter Google Map API
+* @ Developer: Daryl Ferrer Legion
+* @ Email: whoamisnowden@gmail.com
+* @ Github: github.com/whoami15
+* @ Copyright (c) 2016-2017
+* @ License GNU Public Release
+*/
+
+
+<head>
+	<title>Codeigniter Google Map</title>
+	<div id="js">
+		<?php echo $map['js']; ?>
+	</div>
+</head>
+
+<body>
+	<?php echo $map['html']; ?>
+</body>
+
+	
